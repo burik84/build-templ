840042e08f042ec92b605632df1855dcb4959931
burikAV <burikav@yandex.ru>
2019-08-22 00:38:41 +0300

    Исправил работу styles. установил новые плагины для стилей

--- gulpfile.js
+++ gulpfile.js
@@ -39,13 +39,18 @@ let isProd = !isDev;
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const plumber = require('gulp-plumber');
+const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
-const postcss = require('gulp-postcss');
 const del = require('del');
 const browserSync = require('browser-sync').create();
-const concat = require('gulp-concat');
 const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
 const rigger = require('gulp-rigger');
+const gulpif = require('gulp-if');
+const tildeImporter = require('node-sass-tilde-importer');
+
+sass.compiler = require('node-sass');
+
 
 // удаление папки сборки
 function clean() {
@@ -56,19 +61,25 @@ function html() {
     return gulp.src(paths.src.html)
         .pipe(plumber()) // отслеживание ошибок
         .pipe(rigger()) //Прогоним через rigger
+        .pipe(gulpif(isProd, htmlmin({
+            collapseWhitespace: true
+        })))
         .pipe(gulp.dest(paths.build.html)) // выкладывание готовых файлов
         .pipe(browserSync.stream()); // перезагрузка сервера
 }
 
+// Если у нас на входе несколько файлов scss, то plumber и concat я не использую.
 function styles() {
     return gulp.src(paths.src.style) // получим main.scss
-        .pipe(plumber()) // для отслеживания ошибок
-        .pipe(sass()) // scss -> css
-        .pipe(concat('all.css'))
-        .pipe(postcss([autoprefixer({ // добавим префиксы
+        // .pipe(plumber())
+        .pipe(sass({
+            importer: tildeImporter
+        }).on('error', sass.logError)) // scss -> css + импорт из nodemodules c использованием ~
+        // .pipe(concat('all.css'))
+        .pipe(autoprefixer({ // добавим префиксы
             overrideBrowserslist: ['last 2 versions'],
             cascade: false
-        })]))
+        }))
         .pipe(gulpif(isProd, cleanCSS({
             level: 1
         })))
