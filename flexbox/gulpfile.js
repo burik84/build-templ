308a39e8b0e86551804d4778d0b171828697188b
burikAV <burikav@yandex.ru>
2019-08-25 19:26:41 +0300

    практика флекс на навигации

--- flexbox/gulpfile.js
+++ flexbox/gulpfile.js
@@ -1,14 +1,24 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
+
+function clean() {
+    return del('master.css')
+}
 
 function styles() {
-    return gulp.src('css/*.css')
+    return gulp.src('css/master.css')
         .pipe(autoprefixer({
             cascade: false
         }))
         .pipe(gulp.dest('./'))
 }
-exports.styles = styles;
-exports.watch = () => (
+
+function watch() {
     gulp.watch('css/*.css', styles)
-);
+}
+exports.styles = styles;
+exports.clean = clean;
+exports.watch = watch;
+
+gulp.task('default', gulp.series(clean, styles, watch));
