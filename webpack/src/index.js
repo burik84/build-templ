1978e297a750ac2acfe1fbe4dc0d9f4b4c60c117
burikAV <burikav@yandex.ru>
2020-01-04 16:51:28 +0300

    Обновил инструкцию для пользование webpack и добавил шаблоны

---
+++ webpack/src/index.js
@@ -0,0 +1,11 @@
+// Проверяем подключение к проекту
+console.log("Hello webpack");
+
+// Подключение стилей scss
+import './scss/app.scss';
+import './scss/style.scss';
+
+// Подключение стороних плагинов и фреймворков
+import 'jquery';
+import 'popper.js';
+import 'bootstrap';
