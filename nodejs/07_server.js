75475e4f646b48068a1b6a863bc183ba699435f3
burik84 <avburik84@gmail.com>
2022-05-27 00:01:36 +0300

    nodejs: add routing

--- nodejs/07_server.js
+++ nodejs/07_server.js
@@ -1,18 +1,24 @@
 const http = require('http');
-const PORT=3000
-const server=http.createServer((req,res)=>{
+const PORT = 3000;
+const server = http.createServer((req, res) => {
   console.log('Server request');
-  console.log(req.url,req.method);
+  console.log(req.url, req.method);
 
   // res.setHeader('Content-Type','text/plain')
   // res.write('Hello World')
 
-  res.setHeader('Content-Type','text/html')
-  res.write('<h1>Hello World</h1>')
-  res.write('<p>My name is Alex</p>')
-  res.end();
-})
+  // res.setHeader('Content-Type', 'text/html');
+  // res.write('<h1>Hello World</h1>');
+  // res.write('<p>My name is Alex</p>');
 
-server.listen(PORT, 'localhost', (error)=>{
-  error? console.log(error):console.log(`listening port ${PORT}`);;
-})
+  res.setHeader('Content-Type', 'application/json');
+  const data = JSON.stringify([
+    { name: 'Tommy', age: 35 },
+    { name: 'Alex', age: 37 },
+  ]);
+  res.end(data);
+});
+
+server.listen(PORT, 'localhost', (error) => {
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
