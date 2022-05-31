ced1c3cb3490948aee47154c535c4a1ccb2f6176
burik84 <avburik84@gmail.com>
2022-05-31 22:58:17 +0300

    nodejs:add ejs

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,35 +2,60 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+app.set('view engine', 'ejs');
 
 const PORT = 3000;
-const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`);
+const createPath = (page) =>
+  path.resolve(__dirname, 'ejs-views', `${page}.ejs`);
 
 app.listen(PORT, (error) => {
   error ? console.log(error) : console.log(`listening port ${PORT}`);
 });
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
+  const title = 'Home';
   // res.send('Hello world')
-  res.sendFile(createPath('index'))
-})
+  res.render(createPath('index'), { title });
+});
 
-
-app.get('/contacts',(req,res)=>{
+app.get('/contacts', (req, res) => {
+  const title = 'Contacts';
+  const contacts = [
+    { name: 'YouTube', link: 'http://youtube.com/YauhenKavalchuk' },
+    { name: 'Twitter', link: 'http://github.com/YauhenKavalchuk' },
+    { name: 'GitHub', link: 'http://twitter.com/YauhenKavalchuk' },
+  ];
   // res.send('Hello world')
-  res.sendFile(createPath('contacts'))
-})
+  res.render(createPath('contacts'), { contacts, title });
+});
 
-
-app.get('/aboutus',(req,res)=>{
+app.get('/aboutus', (req, res) => {
+  const title = 'About us';
   // res.send('Hello world')
-  res.redirect('/contacts')
-})
+  res.redirect('/contacts');
+});
 
-app.use((req,res)=>{
+app.get('/posts/:id', (req, res) => {
+  // res.send('Hello world')
+  const title = 'Post';
+  res.render(createPath('post'), { title });
+});
+app.get('/posts', (req, res) => {
+  // res.send('Hello world')
+
+  const title = 'Posts';
+  res.render(createPath('posts'), { title });
+});
+app.get('/add-post', (req, res) => {
+  // res.send('Hello world')
+
+  const title = 'Add post';
+  res.render(createPath('add-post'), { title });
+});
+
+app.use((req, res) => {
   // res.statusCode=404
 
-  res
-  .status(404)
-  .sendFile(createPath('error'))
-})
+  const title = 'Error';
+  res.status(404).render(createPath('error'),{title});
+});
